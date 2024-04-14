33f86fafa57c2bae06dc1982f25d0a74710cf8c4
irinadeeva <deeva97@me.com>
2024-04-14 16:50:06 +0200

    add Onboarding

--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -8,14 +8,25 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    
+
     var window: UIWindow?
-    
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
-        
+
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = TabBarController()
+        window?.makeKeyAndVisible()
+
+        let defaults = UserDefaults.standard
+        let isFirstLaunch = defaults.bool(forKey: "isFirstLaunch")
+
+        if !isFirstLaunch {
+            window?.rootViewController = OnboardingPageViewController()
+            defaults.set(true, forKey: "isFirstLaunch")
+        } else {
+            window?.rootViewController = TabBarController()
+        }
+
         window?.makeKeyAndVisible()
     }
 }
